d57440e05ae96e96721029ae652ea51bd0a44c68
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2026-09-25 20:33:07 -0300

    feat: add cpu interrupt handler set

--- interrupt_debouce_gpio/main/interrupt_gpio_debounce.c
+++ interrupt_debouce_gpio/main/interrupt_gpio_debounce.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "esp_rom_sys.h"
+#include "esp_attr.h"
 #include <stdbool.h>
+#include "xtensa_api.h"
 
-#define GPIO_IN_REG (*(volatile uint32_t*)0x3FF4403C) // IN register that we can read state from input pins
 #define GPIO_OUT_W1TC_REG (*(volatile uint32_t*)0x3FF4400C) //  set 1 to a bit clears its function
 #define IO_MUX_GPIO4_REG (*(volatile uint32_t*)0x3FF49048) // IO_MUX pin 4 register
 #define GPIO_OUT_W1TS_REG (*(volatile uint32_t*)0x3FF44008) // turns output pin as HIGH
@@ -11,18 +12,13 @@
 #define GPIO_ENABLE_W1TC_REG (*(volatile uint32_t*)0x3FF44028) // disable pin as output
 #define GPIO_PIN4_REG (*(volatile uint32_t*)0x3FF44098) // configuration for GPIO pin 4
 #define GPIO_STATUS_INT_W1TC (*(volatile uint32_t*)0x3FF4404C) // clear interrupt status
-#define DPORT_APP_GPIO_INTERRUPT_MAP_REG (*(volatile uint32_t*)0x3FF00270) // interrupt matrix register for GPIO interrupts
+#define DPORT_PRO_GPIO_INTERRUPT_MAP_REG (*(volatile uint32_t*)0x3FF0015C) // interrupt matrix register for GPIO interrupts (PRO CPU / core 0)
 
 #define BUTTON_PIN 4
 #define LED_PIN 2
 
 #define FUN_WPU (1 << 8)
 #define FUN_IE  (1 << 9)
-
-bool button_is_pressed() {
-    uint32_t value = GPIO_IN_REG & (1 << BUTTON_PIN);
-    return value == 0;
-}
 
 typedef enum {
     GPIO_LOW = 0,
@@ -41,9 +37,10 @@ bool led_state = false;
 
 volatile bool button_event = false;
 
-static void IRAM_ATTR gpio_isr(void *arg)
-{
+static void IRAM_ATTR gpio_isr(void *arg) {
     button_event = true;
+
+    GPIO_STATUS_INT_W1TC = (1 << BUTTON_PIN); // clear interrupt status for button pin
 }
 
 void app_main(void)
@@ -62,17 +59,13 @@ void app_main(void)
     // ------- INTERRUPT CONFIG
     GPIO_PIN4_REG = (GPIO_PIN4_REG & ~(0x7 << 7)) | (2 << 7); // clear bits 7-9 and sets 2(010) for failing edge trigger
 
-    GPIO_PIN4_REG = (GPIO_PIN4_REG & ~(0x1F << 13)) | (1 << 13); // clear bits 13-17 and sets bit 0 as 1: APP CPU interrupt enable
+    GPIO_PIN4_REG = (GPIO_PIN4_REG & ~(0x1F << 13)) | (1 << 15); // clear bits 13-17 and sets bit 2 as 1: PRO CPU interrupt enable
 
-    DPORT_APP_GPIO_INTERRUPT_MAP_REG = 10; // set CPU interrupt. 10: Peripheral, Edge-Triggered, priority 1(low)
+    DPORT_PRO_GPIO_INTERRUPT_MAP_REG = 10; // set CPU interrupt. 10: Peripheral, Edge-Triggered, priority 1(low)
 
-    gpio_install_isr_service(0);
+    xt_set_interrupt_handler(10, gpio_isr, NULL); // set interrupt handler for GPIO interrupt
 
-    gpio_isr_handler_add(
-        GPIO_NUM_4,
-        gpio_isr,
-        NULL
-    );
+    xt_ints_on(1 << 10); // enable interrupt for GPIO
 
     while (1) {
         if (button_event) {
